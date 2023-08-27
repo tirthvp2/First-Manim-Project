ef115735ee72b4b0a03898a40716c97c82011527
Tirth Patel <tirthvp2@illinois.edu>
2023-08-27 05:21:36 -0500

    Moving Frame Box Works

--- scene.py
+++ scene.py
@@ -1,8 +1,29 @@
+# from manim import *
+#
+#
+# class CreateCircle(Scene):
+#     def construct(self):
+#         circle = Circle()  # create a circle
+#         circle.set_fill(PINK, opacity=0.5)  # set the color and transparency
+#         self.play(Create(circle))  # show the circle on screen
+
 from manim import *
 
 
-class CreateCircle(Scene):
+class MovingFrameBox(Scene):
     def construct(self):
-        circle = Circle()  # create a circle
-        circle.set_fill(PINK, opacity=0.5)  # set the color and transparency
-        self.play(Create(circle))  # show the circle on screen
+        text=MathTex(
+            r"\frac{d}{dx}f(x)g(x)=", r"f(x)\frac{d}{dx}g(x)", r"+",
+            r"g(x)\frac{d}{dx}f(x)"
+        )
+        self.play(Write(text))
+        framebox1 = SurroundingRectangle(text[1], buff = .1)
+        framebox2 = SurroundingRectangle(text[3], buff = .1)
+        self.play(
+            Create(framebox1),
+        )
+        self.wait()
+        self.play(
+            ReplacementTransform(framebox1,framebox2),
+        )
+        self.wait()
